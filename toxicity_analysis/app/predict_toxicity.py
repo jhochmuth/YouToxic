aa6e573e3857c7ff7d96424b7b34ef39b029cf91
jhochmuth <jhochmuth@comcast.net>
2019-03-05 13:38:58 -0700

    Added method to combine model files.

--- toxicity_analysis/app/predict_toxicity.py
+++ toxicity_analysis/app/predict_toxicity.py
@@ -1,8 +1,23 @@
 import torch
 
 
+def create_model():
+    files = list()
+    files.append('model/model_1.pt')
+    files.append('model/model_2.pt')
+    files.append('model/model_3.pt')
+    files.append('model/model_4.pt')
+    files.append('model/model_5.pt')
+
+    with open('model/model.pt', 'wb') as outfile:
+        for file in files:
+            with open(file, 'rb') as infile:
+                outfile.write(infile.read())
+
+
 def predict_toxicity(text):
-    torch.load('model.pt')
+    create_model()
+    torch.load('model/model.pt')
     prediction = 0
     return prediction
 
