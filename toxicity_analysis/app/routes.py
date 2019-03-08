94e7a5ea88fc928ba0a1a27466c2529a32e96346
jhochmuth <jhochmuth@comcast.net>
2019-03-08 16:55:45 -0700

    Using session dictionary.

--- toxicity_analysis/app/routes.py
+++ toxicity_analysis/app/routes.py
@@ -17,8 +17,9 @@ def enter_text():
     if form.validate_on_submit():
         if len(form.text.data.split()) < 3:
             flash('Warning: accuracy of predictions is low for texts with few words.')
+        session['text'] = form.text.data
         session['types'] = form.types.data
-        return redirect(url_for('result', text=form.text.data))
+        return redirect(url_for('result'))
     return render_template('enter_text.html', title='Enter Text', form=form)
 
 
@@ -27,30 +28,38 @@ def enter_twitter_username():
     form = TwitterAccountForm()
     if form.validate_on_submit():
         if validate_username(form.user.data):
-            return redirect(url_for('return_tweets', username=form.user.data, num_tweets=form.num_tweets.data))
+            session['username'] = form.user.data
+            session['num_tweets'] = form.num_tweets.data
+            return redirect(url_for('return_tweets'))
         else:
             flash('Error: twitter account not found with specified username.')
             return redirect(url_for('enter_twitter_username'))
     return render_template('enter_twitter_username.html', title='Enter Twitter Username', form=form)
 
 
-@app.route('/result/<text>')
-def result(text):
+@app.route('/result')
+def result():
     preds = dict()
     classes = dict()
     pred_types = list()
+    text = session['text']
     if 'toxic' in session['types']:
         preds['Toxicity'], classes['Toxicity'] = predict_toxicity(text)
         pred_types.append('Toxicity')
     if 'identity' in session['types']:
         preds['Identity hatred'], classes['Identity hatred'] = predict_identity_hate(text)
         pred_types.append('Identity hatred')
+    session.pop('text', None)
     session.pop('types', None)
     return render_template('result.html', title='Results', text=text, preds=preds, classes=classes, types=pred_types)
 
 
-@app.route('/results_tweets/<username>/<num_tweets>')
-def results_tweets(username, num_tweets):
+@app.route('/results_tweets')
+def results_tweets():
+    username = session['username']
+    num_tweets = session['num_tweets']
+    session.pop('username', None)
+    session.pop('num_tweets', None)
     tweets = get_all_tweets(username, num_tweets=int(num_tweets))
     texts = [row[2] for row in tweets]
     preds, classifications = predict_toxicities(texts)
@@ -59,8 +68,10 @@ def results_tweets(username, num_tweets):
     return render_template('results_tweets.html', title='Results', tweets=tweets)
 
 
-@app.route('/return_tweets/<username>/<num_tweets>')
-def return_tweets(username, num_tweets):
-    tweets = get_all_tweets(username, num_tweets=int(num_tweets))
+@app.route('/return_tweets')
+def return_tweets():
+    username = session['username']
+    num_tweets = session['num_tweets']
+    tweets = get_all_tweets(username, num_tweets=num_tweets)
     return render_template('return_tweets.html', title='Tweets Posted by '+username,
                            tweets=tweets, username=username, num_tweets=num_tweets)
