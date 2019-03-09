9ddbc4c77b12a07b7c858c4bbc6f5bd97d2bc0c4
jhochmuth <jhochmuth@comcast.net>
2019-03-09 13:00:36 -0700

    Began adding comments. Added separate file for neural network definition.

--- toxicity_analysis/app/forms.py
+++ toxicity_analysis/app/forms.py
@@ -5,6 +5,7 @@ from wtforms.validators import DataRequired, NumberRange, Length
 
 
 class EnterTextForm(FlaskForm):
+    """Defines form that requests text for basic predictions."""
     text = StringField('Enter text', validators=[Length(min=2)])
     types = SelectMultipleField('Select classes of toxicity to predict',
                                 choices=[('toxic', 'Toxic'), ('identity', 'Identity hate')])
@@ -12,6 +13,7 @@ class EnterTextForm(FlaskForm):
 
 
 class TwitterAccountForm(FlaskForm):
+    """Defines form that requests information for tweet analysis."""
     user = StringField('Enter twitter username', validators=[DataRequired()])
     num_tweets = IntegerField('Enter number of tweets to classify', validators=[NumberRange(min=1, max=3240)])
     types = SelectMultipleField('Select classes of toxicity to predict',
@@ -20,5 +22,6 @@ class TwitterAccountForm(FlaskForm):
 
 
 class ReturnTweetsForm(FlaskForm):
+    """Defines form that requests display mode for tweet analysis (all or only toxic)."""
     display = SelectField('Select display mode', choices=[('all', 'All'), ('toxic', 'Toxic only')])
     submit = SubmitField('Confirm')
--- toxicity_analysis/app/load_files.py
+++ toxicity_analysis/app/load_files.py
@@ -1,7 +1,3 @@
-import numpy as np
-import pickle
-
-
 def create_model():
     files = list()
     files.append('models/model_1.pt')
@@ -33,35 +29,3 @@ def create_embeddings():
         for file in files:
             with open(file, 'rb') as infile:
                 outfile.write(infile.read())
-
-
-def create_tokenizer():
-    with open('tokenizer.pickle', 'rb') as handle:
-        tokenizer = pickle.load(handle)
-    return tokenizer
-
-
-def sigmoid(x):
-    return 1 / (1 + np.exp(-x))
-
-
-# Note: models were trained when caps_vs_length feature was always 0.
-def get_features(texts):
-    features = list()
-    for text in texts:
-        text = text.lower()
-        length = len(text)
-        capitals = sum(1 for c in text if c.isupper())
-        caps_vs_length = capitals / length
-        num_words = len(text.split())
-        num_unique_words = len(set(text.lower().split()))
-        words_vs_unique = num_unique_words / num_words
-        features.append([caps_vs_length, words_vs_unique])
-    return features
-
-
-def standardize_features(features):
-    with open('scalar.pickle', 'rb') as handle:
-        ss = pickle.load(handle)
-    features = ss.transform(features)
-    return features
