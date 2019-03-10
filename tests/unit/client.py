37ed9b59eadba43c246733a5c37ac8bc5a6bd910
jhochmuth <jhochmuth@comcast.net>
2019-03-09 18:35:33 -0700

    Create tests/unit/client.py.

---
+++ tests/unit/client.py
@@ -0,0 +1,20 @@
+from flask_bootstrap import Bootstrap
+
+import pytest
+
+import youtoxic.app.context as ctx
+from youtoxic.config import Config
+
+
+@pytest.fixture
+def client():
+    app = ctx.create_app()
+    pipeline = ctx.create_pipeline()
+    app.config.from_object(Config)
+    app.config['TESTING'] = True
+    client = app.test_client()
+    bootstrap = Bootstrap(app)
+
+    from youtoxic.app import routes
+
+    yield client
--- tests/unit/test_text_classifications.py
+++ tests/unit/test_text_classifications.py
@@ -1,25 +1,6 @@
-from flask_bootstrap import Bootstrap
-
 from bs4 import BeautifulSoup
 
-import pytest
-
-import youtoxic.app.context as ctx
-from youtoxic.config import Config
-
-
-@pytest.fixture
-def client():
-    app = ctx.create_app()
-    pipeline = ctx.create_pipeline()
-    app.config.from_object(Config)
-    app.config['TESTING'] = True
-    client = app.test_client()
-    bootstrap = Bootstrap(app)
-
-    from youtoxic.app import routes
-
-    yield client
+from tests.unit.client import client
 
 
 def test_get_text_classifications(client):
