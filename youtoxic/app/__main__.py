dd9f7cdd541db23dc11ba2dc4de831a22b273e47
jhochmuth <jhochmuth@comcast.net>
2019-04-05 23:36:09 -0600

    Revert "Test."
    This reverts commit 5f325b3

--- youtoxic/app/__main__.py
+++ youtoxic/app/__main__.py
@@ -14,19 +14,16 @@ def main():
 
 
 @main.command()
-#@click.option('--debug', envvar='DEBUG', default=False, help='debug mode')
-#@click.option('--host', envvar='HOST', default='127.0.0.1', help='host IP address')
-#@click.option('--port', envvar='PORT', default=8050, help='port')
-def runserver(debug, host, port):
+def runserver():
     """Construct the core application."""
     app = Flask(__name__, instance_relative_config=False)
-    bootstrap = Bootstrap(app)  # noqa
-    dash_app = dash_view.add_dash(app)  # noqa
+    bootstrap = Bootstrap(app)
+    dash_app = dash_view.add_dash(app)
 
     # Construct the core application
     app.register_blueprint(routes.main_bp)
 
-    app.run(debug=debug, host=host, port=port)
+    app.run()
 
 
 if __name__ == "__main__":
