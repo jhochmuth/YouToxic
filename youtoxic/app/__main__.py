5f325b34ceeeeb89b8efe782f1a3a04e929bd017
jhochmuth <jhochmuth@comcast.net>
2019-04-05 22:45:45 -0600

    Test.

--- youtoxic/app/__main__.py
+++ youtoxic/app/__main__.py
@@ -14,16 +14,19 @@ def main():
 
 
 @main.command()
-def runserver():
+#@click.option('--debug', envvar='DEBUG', default=False, help='debug mode')
+#@click.option('--host', envvar='HOST', default='127.0.0.1', help='host IP address')
+#@click.option('--port', envvar='PORT', default=8050, help='port')
+def runserver(debug, host, port):
     """Construct the core application."""
     app = Flask(__name__, instance_relative_config=False)
-    bootstrap = Bootstrap(app)
-    dash_app = dash_view.add_dash(app)
+    bootstrap = Bootstrap(app)  # noqa
+    dash_app = dash_view.add_dash(app)  # noqa
 
     # Construct the core application
     app.register_blueprint(routes.main_bp)
 
-    app.run()
+    app.run(debug=debug, host=host, port=port)
 
 
 if __name__ == "__main__":
