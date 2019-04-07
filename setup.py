e5f55d3c520ffd1b8e112021f6108d85edb8f0fb
jhochmuth <jhochmuth@comcast.net>
2019-04-06 19:01:44 -0600

    Setup used correct format for including package data.

--- setup.py
+++ setup.py
@@ -9,12 +9,14 @@ setup(
     description='A Python Dash Server that predicts the toxicity of text using Deep Learning.',
     url='https://github.com/jhochmuth/YouToxic',
     package_data={
-        'youtoxic.app.models': ['*'],
-        'youtoxic.app.utils': ['*.pickle'],
-        'youtoxic.app.assets': ['*'],
-        'youtoxic.app.templates': ['*']
+        'youtoxic': 'youtoxic/app/models/*',
+        'youtoxic': 'youtoxic/app/utils/*.pickle',
+        'youtoxic': 'youtoxic/app/assets/*',
+        'youtoxic': 'youtoxic/app/templates/*'
     },
     include_package_data=True,
+    data_files=[('youtoxic/app/templates', ['index.html']),
+                ('youtoxic/app/assets', ['image.png', 'image-2.png', 'stylesheet.css'])],
     packages=find_packages(exclude=["tests"]),
     license='Apache 2.0',
     long_description=open('README.md').read(),
