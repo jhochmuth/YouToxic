421d958e337d86319a9956fc7723fe93ad7efc0e
jhochmuth <jhochmuth@comcast.net>
2019-04-16 23:30:34 -0600

    Separated get_tweet_predictions() into multiple smaller methods.

--- youtoxic/app/api/tweet_predictions.py
+++ youtoxic/app/api/tweet_predictions.py
@@ -17,6 +17,73 @@ from youtoxic.app.services.tweet_dumper import (
 )
 
 
+def create_graph(tweets, types, preds):
+    """Creates a Graph from tweet data."""
+    y_values = dict()
+
+    if "toxic" in preds:
+        y = list()
+        toxic_tweets = 0
+        for total_tweets, pred in enumerate(reversed(preds["toxic"])):
+            if pred > 0.4:
+                toxic_tweets += 1
+            y.append(toxic_tweets / (total_tweets + 1))
+            total_tweets += 1
+        y_values["Toxicity"] = y
+
+    if "insult" in preds:
+        y = list()
+        insult_tweets = 0
+        for total_tweets, pred in enumerate(reversed(preds["insult"])):
+            if pred > 0.4:
+                insult_tweets += 1
+            y.append(insult_tweets / (total_tweets + 1))
+            total_tweets += 1
+        y_values["Insult"] = y
+
+    if "obscene" in preds:
+        y = list()
+        obscene_tweets = 0
+        for total_tweets, pred in enumerate(reversed(preds["obscene"])):
+            if pred > 0.4:
+                obscene_tweets += 1
+            y.append(obscene_tweets / (total_tweets + 1))
+            total_tweets += 1
+        y_values["Obscenity"] = y
+
+    if "prejudice" in preds:
+        y = list()
+        prejudice_tweets = 0
+        for total_tweets, pred in enumerate(reversed(preds["obscene"])):
+            if pred > 0.4:
+                prejudice_tweets += 1
+            y.append(prejudice_tweets / (total_tweets + 1))
+            total_tweets += 1
+        y_values["Prejudice"] = y
+
+    graph = dcc.Graph(
+        id="toxicity-time",
+        figure={
+            "data": [
+                {
+                    "x": [row[1] for row in reversed(tweets)],
+                    "y": y_values[t],
+                    "name": t,
+                    "line": dict(shape="spline"),
+                }
+                for t in types
+            ],
+            "layout": {
+                "title": "Ratio of Toxic Tweets Over Time",
+                "xaxis": {"title": "Date and Time"},
+                "yaxis": {"title": "Ratio of Toxic Tweets"},
+            },
+        },
+    )
+
+    return graph
+
+
 def create_datatable(df, table_columns):
     """Creates a DataTable from tweet data."""
     table = dash_table.DataTable(
@@ -187,68 +254,7 @@ def get_tweet_predictions(
         table_columns.append(pred)
 
     table = create_datatable(df, table_columns)
-
-    y_values = dict()
-
-    if "toxic" in preds:
-        y = list()
-        toxic_tweets = 0
-        for total_tweets, pred in enumerate(reversed(preds["toxic"])):
-            if pred > 0.4:
-                toxic_tweets += 1
-            y.append(toxic_tweets / (total_tweets + 1))
-            total_tweets += 1
-        y_values["Toxicity"] = y
-
-    if "insult" in preds:
-        y = list()
-        insult_tweets = 0
-        for total_tweets, pred in enumerate(reversed(preds["insult"])):
-            if pred > 0.4:
-                insult_tweets += 1
-            y.append(insult_tweets / (total_tweets + 1))
-            total_tweets += 1
-        y_values["Insult"] = y
-
-    if "obscene" in preds:
-        y = list()
-        obscene_tweets = 0
-        for total_tweets, pred in enumerate(reversed(preds["obscene"])):
-            if pred > 0.4:
-                obscene_tweets += 1
-            y.append(obscene_tweets / (total_tweets + 1))
-            total_tweets += 1
-        y_values["Obscenity"] = y
-
-    if "prejudice" in preds:
-        y = list()
-        prejudice_tweets = 0
-        for total_tweets, pred in enumerate(reversed(preds["obscene"])):
-            if pred > 0.4:
-                prejudice_tweets += 1
-            y.append(prejudice_tweets / (total_tweets + 1))
-            total_tweets += 1
-        y_values["Prejudice"] = y
-
-    graph = dcc.Graph(
-        id="toxicity-time",
-        figure={
-            "data": [
-                {
-                    "x": [row[1] for row in reversed(tweets)],
-                    "y": y_values[t],
-                    "name": t,
-                    "line": dict(shape="spline"),
-                }
-                for t in types
-            ],
-            "layout": {
-                "title": "Ratio of Toxic Tweets Over Time",
-                "xaxis": {"title": "Date and Time"},
-                "yaxis": {"title": "Ratio of Toxic Tweets"},
-            },
-        },
-    )
+    graph = create_graph(tweets, types, preds)
 
     over_max_tweets_message = None
     if num_tweets > 3240:
